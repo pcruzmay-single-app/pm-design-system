cd87b8df1f1bb2a8777d60efcf26e91898e8555a
Pedro Cruz <peter@iMac-de-Pedro.local>
2023-04-07 12:04:12 -0600

    detailing package.json

--- src/pcruzmay-design-system.tsx
+++ src/pcruzmay-design-system.tsx
@@ -2,6 +2,6 @@ export * from "./components/PMAutocomplete";
 export * from "./components/PMTextField";
 export * from "./components/PMAppBar";
 export * from "./components/PMBox";
-export * from "./components/PMIconButton";
+export * from "./components/PMIconButton"
 export * from "./components/PMToolbar";
 export * from "./components/PMTypography";
